b653b3ac3872355d4e27df6031b4894e669b40c5
Alex <atlexeide@gmail.com>
2024-12-08 21:42:09 +0100

    Update filterlogic

--- js/getHandlers.js
+++ js/getHandlers.js
@@ -1,40 +1,46 @@
-import { filterByFood, filterByAllergyFriendly } from "./filters.js";
-export let filtered = [];
-export function getFamilyHandler(req, families) {
-  const parameters = req.query;
-  console.log("filtering");
+import {
+  filterByFood,
+  filterByAllergyFriendly,
+  filterByAlcohol,
+} from "./filters.js";
+export function filterFamilies(parameters, families) {
   console.log("parameters: \n", parameters);
   if (Object.keys(parameters).length === 0)
     throw new Error("no parameters provided");
+  let filtered = families;
   for (const [param, value] of Object.entries(parameters)) {
+    console.log("current param: ", param);
+    console.log(filtered);
     switch (param) {
       case "food":
         console.log("Filtering by food");
-        filterByFood(value, families);
-
-        // semiFiltered.forEach((family) => {
-        //   filtered.push(family);
-        // });
+        filtered = filterByFood(value, filtered);
+        console.log(filtered);
+        break;
+      case "alcohol":
+        console.log("Filtering by alcohol");
+        filtered = filterByAlcohol(value, filtered);
+        console.log(filtered);
         break;
       case "allergy-friendly":
         console.log("Filtering by allergy friendly");
-        filterByAllergyFriendly(value, families);
+        filtered = filterByAllergyFriendly(value, filtered);
         break;
 
       default:
         break;
     }
   }
-  console.log(filtered);
+  return filtered;
 }
 
-function filterFamilies(param, value, families) {
-  families.forEach((family) => {
-    console.log(family.preferences[param]);
-    console.log("VAL", value);
-    if (family.preferences[param] === value) {
-      console.log("match");
-      filtered.push(family);
-    }
-  });
-}
+// function filterFamilies(param, value, families) {
+//   families.forEach((family) => {
+//     console.log(family.preferences[param]);
+//     console.log("VAL", value);
+//     if (family.preferences[param] === value) {
+//       console.log("match");
+//       filtered.push(family);
+//     }
+//   });
+// }
