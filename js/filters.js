62c38865e15c20a3b6939bf32264020cde53891f
Alex <atlexeide@gmail.com>
2024-12-08 21:41:54 +0100

    Update filters

--- js/filters.js
+++ js/filters.js
@@ -1,13 +1,23 @@
-import { filtered } from "./getHandlers.js";
-export function filterByFood(value, families) {
+export function filterByFood(value, families = families) {
+  const filteredList = [];
   families.forEach((family) => {
-    if (family.preferences.food === value) filtered.push(family);
-    // console.log(filtered);
+    if (family.preferences.food === value) filteredList.push(family);
   });
+  return filteredList;
 }
-export function filterByAllergyFriendly(value, families) {
-  const filtered = [];
+export function filterByAlcohol(value, families = families) {
+  const filteredList = [];
   families.forEach((family) => {
-    if (family.preferences["allergy-friendly"] === value) filtered.push(family);
+    if (family.preferences.alcohol.toString() === value)
+      filteredList.push(family);
   });
+  return filteredList;
+}
+export function filterByAllergyFriendly(value, families = families) {
+  const filteredList = [];
+  families.forEach((family) => {
+    if (family.preferences.allergy_friendly.toString() === value)
+      filteredList.push(family);
+  });
+  return filteredList;
 }
