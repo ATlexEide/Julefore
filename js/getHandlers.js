5a39a16e387d6cb54b40e280d7e82bb1e44bfd95
Alex <atlexeide@gmail.com>
2024-12-08 20:34:37 +0100

    Add get handler

--- js/getHandlers.js
+++ js/getHandlers.js
@@ -1,4 +1,29 @@
 export function getFamilyHandler(req, families) {
-  console.log(req);
-  if (!req.query) throw new Error("no parameters provided");
+  const parameters = req.query;
+  console.log("filtering");
+  console.log("parameters: \n", parameters);
+  let filtered = [];
+  if (Object.keys(parameters).length === 0)
+    throw new Error("no parameters provided");
+  for (const [param, value] of Object.entries(parameters)) {
+    switch (param) {
+      case food:
+        filterByFood(value, families);
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+function filterFamilies(param, value, families) {
+  families.forEach((family) => {
+    console.log(family.preferences[param]);
+    console.log("VAL", value);
+    if (family.preferences[param] === value) {
+      console.log("match");
+      filtered.push(family);
+    }
+  });
 }
