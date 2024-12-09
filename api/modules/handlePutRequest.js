89d9f57f18dd388830bf61eb67de0d888e3bcb25
Alex <atlexeide@gmail.com>
2024-12-09 11:08:28 +0100

    Update updateFamily function

--- api/modules/handlePutRequest.js
+++ api/modules/handlePutRequest.js
@@ -1,7 +1,18 @@
 export function updateFamily(req, families) {
-  const family = families.find((family) => {
+  if (!families) throw new Error("NO families provided");
+  let family = families.find((family) => {
     return family.id === Number(req.params.familyId);
   });
   if (!family) throw new Error("Family not found");
-  console.log(family);
+  // console.log(family);
+  for (const [key, value] of Object.entries(req.body)) {
+    if (value.constructor === Object) {
+      for (const [key, secondaryValue] of Object.entries(value)) {
+        console.log(key, secondaryValue);
+      }
+    }
+    // console.log(family);
+    family.key = value;
+  }
+  // console.log(family);
 }
