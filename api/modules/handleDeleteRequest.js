5c7d2c409f8f34762c76e004c266800e2f9a5e71
Alex <atlexeide@gmail.com>
2024-12-09 20:22:51 +0100

    Update delete logic

--- api/modules/handleDeleteRequest.js
+++ api/modules/handleDeleteRequest.js
@@ -1,7 +1,7 @@
 export function deleteFamily(req, families) {
   if (!families) throw new Error("No families provided");
   let family = families.find((family) => {
-    return family.id === Number(req.params.familyId);
+    return family.id.toString() === req.params.familyId;
   });
   if (!family) throw new Error("Family does not exist");
   const familyIndex = families.indexOf(family);
