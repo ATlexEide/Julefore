2a4ad33c614112702a7efdf3e11fc2847874c931
Alex <atlexeide@gmail.com>
2024-12-12 15:38:25 +0100

    Add variable for buttons

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // import { displayFamilies } from "./js/displayFamilies";
-const createFamilyBtn = document.getElementById("create-family-btn");
+const createFamilyBtn = document.getElementById("family-action-btn");
 const actionDialog = document.getElementById("select-action-menu");
 createFamilyBtn.addEventListener("click", () => {
   actionDialog.showModal();
 });
+const createFamilyBtn = document.getElementById("create-family-btn");
+const deleteFamilyBtn = document.getElementById("delete-family-btn");
