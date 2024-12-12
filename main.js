0b981bba3d73adf5772ad17cbb14c8e7ef559069
Alex <atlexeide@gmail.com>
2024-12-12 16:53:09 +0100

    Update button logic

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 // import { displayFamilies } from "./js/displayFamilies";
-const createFamilyBtn = document.getElementById("family-action-btn");
+const openDialogBtn = document.getElementById("family-action-btn");
 const actionDialog = document.getElementById("select-action-menu");
-createFamilyBtn.addEventListener("click", () => {
+const createFamilyDialog = document.getElementById("create-family-dialog");
+openDialogBtn.addEventListener("click", () => {
   actionDialog.showModal();
 });
 const createFamilyBtn = document.getElementById("create-family-btn");
+createFamilyBtn.addEventListener("click", () => {
+  actionDialog.close();
+  createFamilyDialog.showModal();
+});
 const deleteFamilyBtn = document.getElementById("delete-family-btn");
+deleteFamilyBtn.addEventListener("click", () => {
+  console.log("yippeeeee");
+  actionDialog.close();
+});
