f877ee6fff81079302f77882237dbac1f7ff8779
Alex <atlexeide@gmail.com>
2024-12-13 10:35:24 +0100

    Add variables for inputs

---
+++ js/filterPreferences.js
@@ -0,0 +1,7 @@
+const radioInputs = document.getElementsByName("filter-food");
+specifyOtherFoodRadio.value = specifyOtherFoodInput.value;
+const expectsGiftsInput = document.getElementById("filter-gifts");
+const allergyFriendlyInput = document.getElementById("filter-allergy-friendly");
+const alcoholInput = document.getElementById("filter-alcohol");
+const hasPetInput = document.getElementById("filter-pets");
+const applyBtn = document.getElementById("apply-filter-btn");
