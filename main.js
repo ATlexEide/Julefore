a6c5224667125cf4bdf7c43c0a01ceb0bf12942f
Alex <atlexeide@gmail.com>
2024-12-13 12:50:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // import { displayFamilies } from "./js/displayFamilies";
 import { addDialogButtonListeners } from "./js/dialogs.js";
 (() => {
+  console.warn(
+    "PLEASE MAKE SURE API IS RUNNING BY RUNNING 'node api/main.js' IN ROOT"
+  );
   addDialogButtonListeners(document);
 })();
