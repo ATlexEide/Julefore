b68aadd4411ec7d25fe8772314f301a2eac7d418
Alex <atlexeide@gmail.com>
2024-12-13 11:38:58 +0100

    Add frontend logic for filtering

--- js/filterPreferences.js
+++ js/filterPreferences.js
@@ -1,7 +1,21 @@
-const radioInputs = document.getElementsByName("filter-food");
-specifyOtherFoodRadio.value = specifyOtherFoodInput.value;
-const expectsGiftsInput = document.getElementById("filter-gifts");
-const allergyFriendlyInput = document.getElementById("filter-allergy-friendly");
-const alcoholInput = document.getElementById("filter-alcohol");
-const hasPetInput = document.getElementById("filter-pets");
+const inputs = document.getElementsByTagName("input");
 const applyBtn = document.getElementById("apply-filter-btn");
+applyBtn.addEventListener("click", () => {
+  let filter = "?";
+  for (const input of inputs) {
+    if (input.checked) filter += `&${input.name}=${input.value}`;
+  }
+  let filteredFamilies = sendGetFilterReq(filter);
+  console.log(filter);
+  console.log(filteredFamilies);
+});
+
+async function sendGetFilterReq(filter) {
+  try {
+    const req = await fetch(
+      `http://localhost:8080/families/filter/preferences?${filter}`
+    );
+    const res = await req.json();
+    return res;
+  } catch (error) {}
+}
