7f431b5b5a371fadb4a6e8f6b6f99c866833422e
Alex <atlexeide@gmail.com>
2024-12-13 15:17:04 +0100

    Refactor frontend filter logic

--- js/filterPreferences.js
+++ js/filterPreferences.js
@@ -1,20 +1,48 @@
+import { displayFamilies, fetchFamilies } from "./displayFamilies.js";
 const inputs = document.getElementsByTagName("input");
 const applyBtn = document.getElementById("apply-filter-btn");
-applyBtn.addEventListener("click", () => {
-  let filter = "?";
+const removeBtn = document.getElementById("remove-filter-btn");
+
+let filter = "";
+let families = await fetchFamilies();
+displayFamilies(families);
+
+applyBtn.addEventListener("click", async () => {
+  console.log("click");
+  filter = "";
   for (const input of inputs) {
     if (input.checked) filter += `&${input.name}=${input.value}`;
   }
+  console.log("filter length: ", filter.length);
   // TODO: UPDATE AND DISPLAY FAMILIES
-  let filteredFamilies = sendGetFilterReq(filter);
+  if (!filter.length) {
+    return;
+  } else {
+    let filteredFamilies = await updateFamilies(filter);
+    displayFamilies(filteredFamilies);
+  }
 });
-
+removeBtn.addEventListener("click", () => {
+  displayFamilies(families);
+  filter = "";
+  for (const input of inputs) {
+    if (input.checked) input.checked = false;
+  }
+});
+async function updateFamilies(filter) {
+  return await sendGetFilterReq(filter);
+}
 async function sendGetFilterReq(filter) {
+  filter = filter.split("").splice(filter.indexOf("&"));
+  filter.shift();
+  filter = filter.join("");
+  console.log("Filter: ", filter);
   try {
     const req = await fetch(
       `http://localhost:8080/families/filter/preferences?${filter}`
     );
     const res = await req.json();
+    console.log(res);
     return res;
   } catch (error) {}
 }
