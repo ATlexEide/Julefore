d83065caac5d66f8cdf669012c844e82783bc330
Alex <atlexeide@gmail.com>
2024-12-15 18:51:06 +0100

    Refactor eventlistener logic

--- js/displayFamilies.js
+++ js/displayFamilies.js
@@ -5,7 +5,12 @@ export async function fetchFamilies() {
     return res;
   } catch (error) {}
 }
-
+const dialog = document.getElementById("family-dialog");
+const familyPicture = document.getElementById("family-picture");
+const residentsIcon = document.getElementById("icon-residents");
+const giftsIcon = document.getElementById("icon-gifts");
+const foodIcon = document.getElementById("icon-food");
+const alcoholIcon = document.getElementById("icon-alcohol");
 export function displayFamilies(families) {
   console.log("display: ", families);
   const familyList = document.getElementById("two");
@@ -14,6 +19,17 @@ export function displayFamilies(families) {
   families.forEach((family) => {
     const article = document.createElement("article");
     article.classList.add("family-card");
+    article.addEventListener("click", () => {
+      familyPicture.src = family.image;
+      familyPicture.alt = `bilde av ${family.title}`;
+      residentsIcon.textContent = " " + family.preferences.residents;
+      giftsIcon.textContent = family.preferences.gifts
+        ? " Har gaver"
+        : " Har ikke gaver";
+      foodIcon.textContent = family.preferences.food;
+      alcoholIcon.textContent = family.preferences.alcohol ? " Ja" : " Nei";
+      dialog.showModal();
+    });
 
     const figure = document.createElement("figure");
     figure.classList.add("figure-card");
